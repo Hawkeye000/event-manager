f6fb312e78dabc06ce4d3052775e6340a4ddea3f
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-02 21:44:47 -0400

    added outputting form letters and generated output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -11,29 +12,31 @@ def legislators_by_zipcode(zipcode)
 
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislators_string = legislator_names.join(", ")
-
 end
 
 puts "EventManager Initialized"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
-template_letter = File.read "form_letter.html"
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('First_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-  
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+
 end
