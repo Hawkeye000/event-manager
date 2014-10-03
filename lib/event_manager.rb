b566bd5a27172fccde94c0604e6bc0dff145c2f2
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-02 20:22:16 -0400

    refactored clean_zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,7 @@
 require "csv"
 
 def clean_zipcode(zipcode)
-  if zipcode.nil? 
-    zipcode = "00000"
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust 5, '0'
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 puts "EventManager Initialized"
@@ -19,6 +11,6 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
-  
+
   puts "#{name} #{zipcode}"
 end
